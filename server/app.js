ec9a603d9e5e3ba18d7a347337c8162014cbd1b1
Ashish singh <ashish.singhsuttle@gmail.com>
2025-05-03 23:52:53 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -10,12 +10,7 @@ const { addtask } = require("./services/task");
 
 app.use(express.json());
 
-app.use(cors({
-    origin: "http://localhost:5173",  // Not an array, just a string
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}));   
+app.use(cors());   
 app.use(cookieParser()); 
 
 app.get("/", (req, res) => {
