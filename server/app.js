34e747f1260516eb678936986006c40a64538058
Ashish singh <ashish.singhsuttle@gmail.com>
2025-05-04 00:19:33 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -10,7 +10,14 @@ const { addtask } = require("./services/task");
 
 app.use(express.json());
 
-app.use(cors());   
+app.use(cors({
+  origin: true,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+}));
+
+app.options('*', cors());
+
 app.use(cookieParser()); 
 
 app.get("/", (req, res) => {
